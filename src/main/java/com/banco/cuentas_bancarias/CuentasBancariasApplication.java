757c9cb18c03da3659b7b600e05dc98627fbb89f
GabyB73 <geb.beg.73@gmail.com>
2024-10-22 18:14:23 +0200

    Inicializando el proyecto

---
+++ src/main/java/com/banco/cuentas_bancarias/CuentasBancariasApplication.java
@@ -0,0 +1,13 @@
+package com.banco.cuentas_bancarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentasBancariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentasBancariasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banco/cuentas_bancarias/CuentasBancariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.cuentas_bancarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentasBancariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
